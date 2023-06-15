ab2e8c0a55cc28d89779572bf65ca23512e17bc8
HadiSArab <h.safararab@gmail.com>
2023-06-15 12:23:06 +0330

    third stop

--- divar_buy_apartment.py
+++ divar_buy_apartment.py
@@ -9,6 +9,8 @@ def product(i):
         
     # dictionary to store nested json
     dic = {}
+    list_data_loc = 0
+    tags_loc = 0
 
     # url is a variable to store desired api
     # this request response page number api of digikala , counter start from "0" but page number start from "1".beacuse of that i changed format to "counter + 1"
@@ -16,64 +18,49 @@ def product(i):
 
     resp = requests.get(url)
     resp = json.loads(resp.text)
-    temp = resp['sections'][5]['widgets'][0]['data']['chip_list']['chips'][1]['action']['payload']['web_url']
+
+    for i in range(len(resp['sections'])):
+        if resp['sections'][i]['section_name'] == "TAGS":
+            tags_loc = i
+
+        if resp['sections'][i]['section_name'] == "LIST_DATA":
+            list_data_loc = i
+
+
+    temp = resp['sections'][tags_loc]['widgets'][0]['data']['chip_list']['chips'][1]['action']['payload']['web_url']
     temp1 = temp.split('/')
 
     dic['city']     = temp1[0]
     dic['category'] = temp1[1]
     dic['district'] = temp1[2]
 
-    if len(resp['sections']) == 10:
-        dic['size']     = resp['sections'][9]['widgets'][0]['data']['items'][0]['value']
-        dic['year of construction'] = resp['sections'][9]['widgets'][0]['data']['items'][1]['value']
-        dic['number of rooms']      = resp['sections'][9]['widgets'][0]['data']['items'][2]['value'] 
-        dic['floor']      = resp['sections'][9]['widgets'][5]['data']['value'].split(" ")[0]
 
-        if resp['sections'][9]['widgets'][7]['data']['items'][0]['available']:
-            dic['elevator'] = 'yes'
-        else:
-            dic['elevator'] = 'no'
+    dic['size']     = resp['sections'][list_data_loc]['widgets'][0]['data']['items'][0]['value']
+    dic['year of construction'] = resp['sections'][list_data_loc]['widgets'][0]['data']['items'][1]['value']
+    dic['number of rooms']      = resp['sections'][list_data_loc]['widgets'][0]['data']['items'][2]['value'] 
+    dic['floor']      = resp['sections'][list_data_loc]['widgets'][5]['data']['value'].split(" ")[0]
 
-
-        if resp['sections'][9]['widgets'][7]['data']['items'][1]['available']:
-            dic['parking'] = 'yes'
-        else:
-            dic['parking'] = 'no'
-
-
-        if resp['sections'][9]['widgets'][7]['data']['items'][2]['available']:
-            dic['storage'] = 'yes'
-        else:
-            dic['storage'] = 'no'
-
-        dic['price per meter']      = resp['sections'][9]['widgets'][2]['data']['value']
-        dic['total price']      = resp['sections'][9]['widgets'][1]['data']['value']
-
+    if 'available' in resp['sections'][list_data_loc]['widgets'][7]['data']['items'][0].keys():
+        dic['elevator'] = 'yes'
     else:
-        dic['size']     = resp['sections'][8]['widgets'][0]['data']['items'][0]['value']
-        dic['year of construction'] = resp['sections'][8]['widgets'][0]['data']['items'][1]['value']
-        dic['number of rooms']      = resp['sections'][8]['widgets'][0]['data']['items'][2]['value'] 
-        dic['floor']      = resp['sections'][8]['widgets'][5]['data']['value'].split(" ")[0]
-
-        if resp['sections'][8]['widgets'][7]['data']['items'][0]['available']:
-            dic['elevator'] = 'yes'
-        else:
-            dic['elevator'] = 'no'
+        dic['elevator'] = 'no'
 
 
-        if resp['sections'][8]['widgets'][7]['data']['items'][1]['available']:
-            dic['parking'] = 'yes'
-        else:
-            dic['parking'] = 'no'
+    if 'available' in resp['sections'][list_data_loc]['widgets'][7]['data']['items'][1].keys():
+        dic['parking'] = 'yes'
+    else:
+        dic['parking'] = 'no'
 
 
-        if resp['sections'][8]['widgets'][7]['data']['items'][2]['available']:
-            dic['storage'] = 'yes'
-        else:
-            dic['storage'] = 'no'
-            
-        dic['price per meter']      = resp['sections'][8]['widgets'][2]['data']['value']
-        dic['total price']      = resp['sections'][8]['widgets'][1]['data']['value']
+    if 'available' in resp['sections'][list_data_loc]['widgets'][7]['data']['items'][2].keys():
+        dic['storage'] = 'yes'
+    else:
+        dic['storage'] = 'no'
+
+    dic['price per meter']      = resp['sections'][list_data_loc]['widgets'][2]['data']['value']
+    dic['total price']      = resp['sections'][list_data_loc]['widgets'][1]['data']['value']
+
+    
     
     return dic
 
