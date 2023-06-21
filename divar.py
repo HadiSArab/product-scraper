b1cd31189dc3d1a877b2bf0f6299e11378c0e57b
hadi <h.safararab@gmail.com>
2023-06-21 13:51:29 +0330

    add id and date to json. create data frame and save CSV file

--- divar.py
+++ divar.py
@@ -1,11 +1,9 @@
+import pandas as pd
+from unidecode import unidecode
 
-# scrape data and store all product keys and values
-import json,requests
 
-# url is a variable to store desired api
-# this request response page number api of digikala , counter start from "0" but page number start from "1".beacuse of that i changed format to "counter + 1"
-url = 'https://api.divar.ir/v8/posts-v2/web/AZJGY6xy'
+df = pd.read_json('./divar.json')
+df = df.T
+print(df.columns)
 
-resp = requests.get(url)
-resp = json.loads(resp.text)
-print(resp)
+df.to_csv('./divar.csv')
--- divar_buy_apartment.py
+++ divar_buy_apartment.py
@@ -2,9 +2,21 @@
 # scrape data and store all product keys and values
 import json,requests
 import time 
+from datetime import datetime
+import os.path
 
 # this is last dictionary to store
 main_dic ={}
+
+if os.path.isfile('D:\Projects\PYTHON\electrical-product-scraper\divar.json'):
+    f = open('D:\Projects\PYTHON\electrical-product-scraper\divar.json')
+    main_dic = json.load(f)
+
+
+# current datetime
+now = datetime.now()
+current_date = now.date()
+
 
 def product(i):
         
@@ -17,7 +29,7 @@ def product(i):
     # this request response page number api of digikala , counter start from "0" but page number start from "1".beacuse of that i changed format to "counter + 1"
     url = 'https://api.divar.ir/v8/posts-v2/web/{}'.format(i)
 
-    resp = requests.get(url)
+    resp = requests.get(url,timeout=60)
     resp = json.loads(resp.text)
 
     for i in range(len(resp['sections'])):
@@ -32,6 +44,7 @@ def product(i):
     temp = resp['sections'][tags_loc]['widgets'][0]['data']['chip_list']['chips'][1]['action']['payload']['web_url']
     temp1 = temp.split('/')
 
+    dic['id'] = resp['webengage']['token']
     dic['city']     = temp1[0]
     dic['category'] = temp1[1]
     dic['district'] = temp1[2]
@@ -59,6 +72,7 @@ def product(i):
     else:
         dic['storage'] = 'no'
 
+    dic['date'] = str(current_date)
     dic['price per meter']      = resp['sections'][list_data_loc]['widgets'][2]['data']['value']
     dic['total price']      = resp['sections'][list_data_loc]['widgets'][1]['data']['value']
 
@@ -66,30 +80,34 @@ def product(i):
     
     return dic
 
-
 a = 0
+t = len(main_dic)
+
 while True:
     
     x = []
 
     # url = 'https://api.divar.ir/v8/web-search/tehran/buy-apartment'
     url = 'https://api.divar.ir/v8/web-search/tehran/buy-apartment?has-photo=true&sort=sort_date'
-    resp = requests.get(url)
+    resp = requests.get(url,timeout=60)
     resp = json.loads(resp.text)
 
     for i in range(23):
         x.append(resp['web_widgets']['post_list'][i]['data']['token'])
 
-    s = 0
+
+    
+
     for i in x:
         print(i)
-        main_dic[(23*a)+s] = product(i)
-        with open ('divar.json','w') as l:
+        main_dic[a+t] = product(i)
+        with open ('D:\Projects\PYTHON\electrical-product-scraper\divar.json','w') as l:
             json.dump(main_dic,l)
-        s+=1 
+        a+=1 
+        print(a)
         time.sleep(2)   
 
-    a+=1
+    
     time.sleep(60)
 
 
